28493b4efeb1d33c44dacca026ac33400d5d5a89
harshathoom-tech <harsha.jdeveloper@gmail.com>
2025-07-09 00:29:32 -0500

    Update Student.java

--- input2/student-management/src/main/java/com/example/studentmanagement/model/Student.java
+++ input2/student-management/src/main/java/com/example/studentmanagement/model/Student.java
@@ -1,38 +1,38 @@
-public class Student {
-    private Long id;
-    private String name;
-    private int age;
-    private String grade;
+// public class Student {
+//     private Long id;
+//     private String name;
+//     private int age;
+//     private String grade;
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
 
-    public int getAge() {
-        return age;
-    }
+//     public int getAge() {
+//         return age;
+//     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+//     public void setAge(int age) {
+//         this.age = age;
+//     }
 
-    public String getGrade() {
-        return grade;
-    }
+//     public String getGrade() {
+//         return grade;
+//     }
 
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-}
+//     public void setGrade(String grade) {
+//         this.grade = grade;
+//     }
+// }
